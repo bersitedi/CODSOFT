da05e73df71b118551f0a0a4d20782739f6ca93c
bersitedi <bersitedi421@gmail.com>
2024-03-12 23:23:09 +0100

    create article cards for our home page

---
+++ frontend/src/components/ArticleCard.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import pudding from "../assets/photo1.jpg";
+import defaultprofile from "../assets/defaultpfp.jpg";
+import { BsCheckLg } from "react-icons/bs";
+
+const ArticleCard = ({ className }) => {
+  return (
+    <div className={`rounded-xl overflow-hidden shadow-md ${className}`}>
+      <img
+        src={pudding}
+        alt="title"
+        className="w-full object-cover object-center h-60 lg:h-48 "
+      />
+      <div className="p-5">
+        <h2 className="font-roboto font-bold text-l text-dark-soft md:text-xl">
+          Yogurt and Chia Seed Pudding
+        </h2>
+        <p className="text-dark-soft mt-2 text-sm ">
+          Healthy Breakfast Ideas to Start Your Day Right
+        </p>
+        <div className="flex justify-between flex-nowrap items-center mt-5">
+          <div className="flex items-center gap-x-2 md:gap-x-2.5">
+            <img
+              src={defaultprofile}
+              alt=""
+              className="rounded-full w-10 h-10 "
+            />
+            <div className="flex flex-col">
+              <h4 className="font-bold italic text-dark-soft text-sm ">
+                Bersi Tedi
+              </h4>
+              <div className="flex items-center gap-x-1 gap-y-3">
+                <span className="bg-green-300 w-fit bg-opacity-20 p-1.5 rounded-full">
+                  <BsCheckLg className="text-green-500 w-3 h-3" />
+                </span>
+                <span className="italic text-dark-soft text-xs ">
+                  Verified writer
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <span className="font-bold text-dark-soft italic text-sm ">
+            02 May
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ArticleCard;
--- frontend/src/pages/container/HeroPage.jsx
+++ frontend/src/pages/container/HeroPage.jsx
@@ -4,21 +4,22 @@ import { FiSearch } from "react-icons/fi";
 
 const HeroPage = () => {
   return (
-    <main className="container flex flex-col mx-auto px-5 py-5 lg:flex-row">
+    <main className="container flex flex-col mx-auto px-5 py-5  lg:flex-row">
       <div className="mt-8 lg:w-1/2">
         <h1 className="text-4xl font-bold mb-4 text-center text-dark-soft lg:text:5xl md:text-left lg:max-w-[540px]">
-          Welcome to Our Website
+          Welcome to TastyBites,
         </h1>
-        <p className="text-lg mb-4 text-dark-eerie text-center md:text-left md:text-xl">
-          Discover amazing content and features.
+        <p className="text-lg mb-4 text-dark-eerie text-center md:text-left md:text-xl lg:text-base">
+          Explore Exciting Recipes Discover a vast collection of recipes curated
+          by food enthusiasts and chefs from around the world.
         </p>
-        <div className="flex flex-col md:flex-row lg:flex-row md:items-center lg:items-start  lg:justify-start">
+        <div className="flex flex-col md:flex-row lg:flex-row md:items-center lg:items-start lg:mt-7 xl:mt-10 lg:justify-start">
           <div className="flex items-center shadow-md mb-4 md:mb-0 lg:mb-0">
             <FiSearch className="text-gray-400 w-6 h-6 ml-3" />
             <input
               type="text"
               placeholder="Search..."
-              className="w-full px-4 py-1 border-none  focus:outline-none ml-1"
+              className="w-full px-4 py-1 border-none focus:outline-none ml-1"
             />
             <button className="hidden sm:flex bg-blue-500 text-white px-6 py-1 rounded-md ml-3 hover:bg-blue-600 shadow-md">
               Explore
@@ -28,11 +29,11 @@ const HeroPage = () => {
             Explore
           </button>
         </div>
-        <div className="flex flex-col mt-5 lg:items-start lg:flex-row lg:flex-nowrap lg:gap-x-1 lg:mt-7">
-          <span className="text-dark-night font-semibold italic ">
+        <div className="flex flex-col mt-5 lg:items-start lg:flex-row lg:flex-nowrap lg:gap-x-4 lg:mt-7">
+          <span className="text-dark-night font-semibold italic lg:text-sm mt-2 lg:mt-4 ">
             Popular tags:
           </span>
-          <ul className="flex flex-wrap gap-x-3 gap-y-2 mt-1">
+          <ul className="flex flex-wrap gap-x-2.5 gap-y-2.5 mt-3 lg:text-sm">
             <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-sans">
               FAQ Item 1
             </li>
