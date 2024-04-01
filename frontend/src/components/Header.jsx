bf744a228e9092bd48d5bb9c8926301c8988450c
bersitedi <bersitedi421@gmail.com>
2024-04-02 00:22:03 +0200

    create log out functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,10 +6,18 @@ import {
   AiOutlineRight,
 } from "react-icons/ai";
 import logo from "../assets/tech-blog.png";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../store/actions/user";
 
 const Header = ({ navItemsList }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const userState = useSelector((state) => state.user);
+  const [profileDrowpdown, setProfileDrowpdown] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -20,6 +28,10 @@ const Header = ({ navItemsList }) => {
   };
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const logoutHandler = () => {
+    dispatch(logout());
   };
 
   return (
@@ -64,7 +76,7 @@ const Header = ({ navItemsList }) => {
                             <a
                               key={subIndex}
                               href="#"
-                              className="block px-4 py-2 hover:bg-gray-100"
+                              className="block px-4 py-2 hover:bg-blue-500 hover:text-white"
                             >
                               {subItem}
                             </a>
@@ -76,10 +88,60 @@ const Header = ({ navItemsList }) => {
                 </div>
               ))}
             </div>
+            {userState.userInfo ? (
+              <div className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
+                <div className="relative group">
+                  <div className="flex flex-col items-center">
+                    <button
+                      className="flex gap-x-1 items-center mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300"
+                      onClick={() => setProfileDrowpdown(!profileDrowpdown)}
+                    >
+                      <span>Account</span>
+                      <MdKeyboardArrowDown />
+                    </button>
+                    <div
+                      className={`${
+                        profileDrowpdown ? "block" : "hidden"
+                      } lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
+                    >
+                      <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
+                        {userState?.userInfo?.admin && (
+                          <button
+                            onClick={() => navigate("/admin")}
+                            type="button"
+                            className="hover:bg-dark-hard hover:text-white px-4 py-2 text-dark-soft "
+                          >
+                            Admin Dashboard
+                          </button>
+                        )}
 
-            <button className="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600">
-              Sign In
-            </button>
+                        <button
+                          onClick={() => navigate("/profile")}
+                          type="button"
+                          className="hover:bg-blue-500 hover:text-white px-4 py-2 text-dark-soft"
+                        >
+                          Profile Page
+                        </button>
+                        <button
+                          onClick={logoutHandler}
+                          type="button"
+                          className="hover:bg-blue-500 hover:text-white px-4 py-2 text-dark-soft"
+                        >
+                          Logout
+                        </button>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <button
+                onClick={() => navigate("/login")}
+                className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300"
+              >
+                Sign in
+              </button>
+            )}
           </div>
         </div>
 
@@ -139,8 +201,11 @@ const Header = ({ navItemsList }) => {
                     )}
                   </div>
                 ))}
-                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                  Sign In
+                <button
+                  onClick={() => navigate("/login")}
+                  className="mt-5 lg:mt-0 border-2 border-white px-6 py-2 rounded-md text-white font-semibold transition-all duration-300"
+                >
+                  Sign in
                 </button>
               </div>
             </div>
