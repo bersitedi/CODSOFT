aa3db7d773c77b43aa64a64b5ed78d594f239b27
bersitedi <bersitedi421@gmail.com>
2024-04-16 03:10:35 +0200

    render our similar posts dynamically

--- frontend/src/pages/articleDetail/ArticleDetailPage.jsx
+++ frontend/src/pages/articleDetail/ArticleDetailPage.jsx
@@ -6,7 +6,7 @@ import SimilarPosts from "./container/SimilarPosts";
 import { images, stables } from "../../constant";
 import SocialShareButtons from "../../components/SocialShareButtons";
 import { useQuery } from "@tanstack/react-query";
-import { getSinglePost } from "../../services/index/posts";
+import { getAllPosts, getSinglePost } from "../../services/index/posts";
 import { generateHTML } from "@tiptap/html";
 import Bold from "@tiptap/extension-bold";
 // Option 2: Browser-only (lightweight)
@@ -16,41 +16,15 @@ import Paragraph from "@tiptap/extension-paragraph";
 import Text from "@tiptap/extension-text";
 import Italic from "@tiptap/extension-italic";
 import parse from "html-react-parser";
+import ArticleDetailSkeleton from "./component/ArticleDetailSkeleton";
+import ErrorMessage from "../../components/ErrorMessage";
 
-const postData = [
-  {
-    _id: "1",
-    image: images.Post1Image,
-    title: "Learn how to cook healthy meals",
-    createdAt: "2024-04-26",
-  },
-  {
-    _id: "2",
-    image: images.Post1Image,
-    title: "Learn how to cook healthy meals",
-    createdAt: "2024-04-26",
-  },
-  {
-    _id: "3",
-    image: images.Post1Image,
-    title: "Learn how to cook healthy meals",
-    createdAt: "2024-04-26",
-  },
-  {
-    _id: "4",
-    image: images.Post1Image,
-    title: "Learn how to cook healthy meals",
-    createdAt: "2024-04-26",
-  },
-];
-
-const tagsData = ["Cooking", "Lifestyle", "Healthy", "Food", "Diet", "Learn"];
 const ArticleDetailPage = ({ post }) => {
   const { slug } = useParams();
   const [breadCrumbsData, setBreadCrumbsData] = useState([]);
   const [body, setBody] = useState(null);
 
-  const { data } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["Project", slug],
     queryFn: () => getSinglePost({ slug }),
     onSuccess: (data) => {
@@ -66,58 +40,64 @@ const ArticleDetailPage = ({ post }) => {
       );
     },
   });
+  const { data: postsData } = useQuery({
+    queryFn: () => getAllPosts(),
+    queryKey: ["posts"],
+  });
   return (
     <MainLayout>
-      <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
-        <article className="flex-1">
-          <BreadCrumbs data={breadCrumbsData} />
-          <img
-            src={
-              data?.photo
-                ? stables.UPLOAD_FOLDER_BASE_URL | data?.photo
-                : images.Post1Image
-            }
-            className="w-full rounded-xl"
-            alt={data?.title}
-          />
-          <div className="mt-4 flex gap-2">
-            {data?.categories.map((category) => (
-              <Link
-                to={`/project?category=${category.name}`}
-                className="text-primary text-sm font-roboto inline-block md:text-base"
-              >
-                {category.name}
-              </Link>
-            ))}
-          </div>
-
-          <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]">
-            {data?.title}
-          </h1>
-          <div className="mt-4 prose prose-sm sm:prose-base">{body}</div>
-        </article>
-        <div>
-          <SimilarPosts
-            header="Latest Article"
-            posts={postData}
-            tags={tagsData}
-            className="mt-8 lg:mt-0 lg:max-w-xs"
-          />
-          <div className="mt-7">
-            <h2 className="font-roboto font-medium text-dark-hard mb-4 md:text-xl">
-              Share on:
-            </h2>
-            <SocialShareButtons
-              url={encodeURI(
-                "https://moonfo.com/post/client-side-and-server-side-explanation"
-              )}
-              title={encodeURIComponent(
-                "Client-side and server-side explanation"
-              )}
+      {isLoading ? (
+        <ArticleDetailSkeleton />
+      ) : isError ? (
+        <ErrorMessage message="Something went wrong, Couldn't fetch the post detail" />
+      ) : (
+        <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
+          <article className="flex-1">
+            <BreadCrumbs data={breadCrumbsData} />
+            <img
+              src={
+                data?.photo
+                  ? stables.UPLOAD_FOLDER_BASE_URL | data?.photo
+                  : images.Post1Image
+              }
+              className="w-full rounded-xl"
+              alt={data?.title}
             />
+            <div className="mt-4 flex gap-2">
+              {data?.categories.map((category) => (
+                <Link
+                  to={`/project?category=${category.name}`}
+                  className="text-primary text-sm font-roboto inline-block md:text-base"
+                >
+                  {category.name}
+                </Link>
+              ))}
+            </div>
+
+            <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]">
+              {data?.title}
+            </h1>
+            <div className="mt-4 prose prose-sm sm:prose-base">{body}</div>
+          </article>
+          <div>
+            <SimilarPosts
+              header="Latest Article"
+              posts={postsData}
+              tags={data?.tags}
+              className="mt-8 lg:mt-0 lg:max-w-xs"
+            />
+            <div className="mt-7">
+              <h2 className="font-roboto font-medium text-dark-hard mb-4 md:text-xl">
+                Share on:
+              </h2>
+              <SocialShareButtons
+                url={encodeURI(window.location.href)}
+                title={encodeURIComponent(data?.title)}
+              />
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </MainLayout>
   );
 };
--- frontend/src/pages/container/Articles.jsx
+++ frontend/src/pages/container/Articles.jsx
@@ -4,6 +4,9 @@ import { FaArrowRight } from "react-icons/fa";
 import { useQuery } from "@tanstack/react-query";
 import { getAllPosts } from "../../services/index/posts";
 import { toast } from "react-hot-toast";
+import ArticleCardSkeleton from "../../components/ArticleCardSkeleton";
+import ErrorMessage from "../../components/ErrorMessage";
+import { Link } from "react-router-dom";
 
 const Articles = () => {
   const { data, isLoading, isError } = useQuery({
@@ -16,24 +19,34 @@ const Articles = () => {
   });
 
   return (
-    <section className="container mx-auto px-2 py-10 flex flex-col mt-10 lg:flex-row lg:gap-10">
-      <div className="lg:pr-6">
-        <div className="flex flex-wrap justify-between md:gap-x-5 gap-y-5 pb-10">
-          {!isLoading &&
-            !isError &&
-            data.map((post) => (
-              <ArticleCard
-                key={post._id}
-                post={post}
-                className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]"
-              />
-            ))}
-        </div>
-        <button className="flex items-center gap-x-2 mx-auto font-bold text-primary border-2 border-primary px-6 py-1 rounded-lg hover:text-white hover:bg-primary shadow-md ">
-          <span>More articles</span>
-          <FaArrowRight className="w-3 h-3" />
-        </button>
+    <section className="flex flex-col container mx-auto px-5 py-10">
+      <div className=" flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
+        {isLoading ? (
+          [...Array(3)].map((item, index) => (
+            <ArticleCardSkeleton
+              key={index}
+              className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]"
+            />
+          ))
+        ) : isError ? (
+          <ErrorMessage message="Something went wrong, Couldn't fetch the posts data" />
+        ) : (
+          data.map((post) => (
+            <ArticleCard
+              key={post._id}
+              post={post}
+              className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]"
+            />
+          ))
+        )}
       </div>
+      <Link
+        to=""
+        className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-1 rounded-lg hover:bg-cta hover:text-white"
+      >
+        <span>More articles</span>
+        <FaArrowRight className="w-3 h-3" />
+      </Link>
     </section>
   );
 };
