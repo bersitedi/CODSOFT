b8594dda7804f5c5bb4dc9d598e33430be82a564
bersitedi <bersitedi421@gmail.com>
2024-04-21 04:11:35 +0200

    update header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,33 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
-import logo from "../assets/logo.jpg";
-import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../store/actions/user";
+import logo from "../assets/logo.jpg";
 
-const navItemsInfo = [
-  { name: "Home", type: "link", href: "/" },
-  { name: "About", type: "link", href: "/about" },
-  {
-    name: "Projects",
-    type: "dropdown",
-    items: [
-      { title: "Project1", href: "/projects" },
-      { title: "Project2", href: "/projects" },
-      { title: "Project3", href: "/projects" },
-      { title: "Project4", href: "/projects" },
-      { title: "Project5", href: "/projects" },
-      { title: "Project6", href: "/projects" },
-      { title: "Project7", href: "/projects" },
-      { title: "Project8", href: "/projects" },
-    ],
-  },
-  { name: "Service", type: "link", href: "/services" },
-  { name: "Contact", type: "link", href: "/contact" },
-];
 const NavItem = ({ item }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const navigate = useNavigate();
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
@@ -41,45 +23,59 @@ const NavItem = ({ item }) => {
     setIsDropdownOpen(false);
   };
 
+  const handleDropdownMouseEnter = () => {
+    setIsDropdownOpen(true);
+  };
+
+  const handleDropdownMouseLeave = () => {
+    setIsDropdownOpen(false);
+  };
+
   return (
-    <li className="relative group">
-      {item.type === "link" ? (
-        <Link
-          to={item.href}
-          className="px-4 py-2 transition-colors duration-300 hover:text-blue-500 hover:border-b-2 hover:border-blue-500"
-        >
+    <li
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className={`relative ${item.type === "dropdown" ? "group" : ""}`}
+    >
+      {item.type === "link" && (
+        <a href={item.href} className="hover:text-blue-500">
           {item.name}
-        </Link>
-      ) : (
-        <div className="relative">
+        </a>
+      )}
+      {item.type === "dropdown" && (
+        <div
+          onMouseEnter={handleDropdownMouseEnter}
+          onMouseLeave={handleDropdownMouseLeave}
+        >
           <button
-            className="px-4 py-2 flex gap-x-1 items-center"
             onClick={toggleDropdown}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
+            className="flex items-center justify-between w-full px-4 py-2 text-left"
           >
-            <span>{item.name}</span>
-            {isDropdownOpen ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}
+            {item.name}
+            {isDropdownOpen ? (
+              <MdKeyboardArrowDown className="w-5 h-5" />
+            ) : (
+              <MdKeyboardArrowUp className="w-5 h-5" />
+            )}
           </button>
           {isDropdownOpen && (
-            <div
-              className="lg:hidden lg:bg-white bg-cta transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max rounded-md"
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
+            <ul
+              className="absolute left-0  bg-white rounded-lg shadow-lg"
+              onMouseEnter={handleDropdownMouseEnter}
+              onMouseLeave={handleDropdownMouseLeave}
             >
-              <ul className="flex flex-col text-center">
-                {item.items.map((page, index) => (
-                  <li key={index}>
-                    <Link
-                      to={page.href}
-                      className="block px-3 py-2 hover:bg-gray-200 rounded-md"
-                    >
-                      {page.title}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
+              {item.items.map((subItem, index) => (
+                <li key={index}>
+                  <Link
+                    to={`/projects?category=${subItem.category}`}
+                    className="block px-4 py-2 text-sm text-gray-700
+                    hover:bg-blue-500 hover:text-white"
+                  >
+                    {subItem.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
           )}
         </div>
       )}
@@ -88,24 +84,57 @@ const NavItem = ({ item }) => {
 };
 
 const Header = () => {
+  const [navItems, setNavItems] = useState([
+    { name: "Home", type: "link", href: "/" },
+    { name: "About", type: "link", href: "/about" },
+    { name: "Projects", type: "dropdown", items: [] },
+    { name: "Service", type: "link", href: "/services" },
+    { name: "Contact", type: "link", href: "/contact" },
+  ]);
   const [navIsVisible, setNavIsVisible] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const userState = useSelector((state) => state.user);
-  const [profileDrowpdown, setProfileDrowpdown] = useState(false);
+  const [profileDropdown, setProfileDropdown] = useState(false);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get("/api/post-categories");
+        const categories = response.data;
+        const updatedNavItems = navItems.map((item) => {
+          if (item.name === "Projects") {
+            return {
+              ...item,
+              items: categories.map((category) => ({
+                title: category.title,
+                href: `/projects/${category._id}`,
+              })),
+            };
+          }
+          return item;
+        });
+        setNavItems(updatedNavItems);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
 
   const navVisibilityHandler = () => {
-    setNavIsVisible((curState) => {
-      return !curState;
-    });
+    setNavIsVisible((curState) => !curState);
   };
+
   const logoutHandler = () => {
     dispatch(logout());
   };
+
   return (
     <section className="sticky top-0 left-0 right-0 z-50 bg-white">
-      <header className="container  mx-auto px-5 flex justify-between py-4 items-center">
-        <Link to="/" className="flex items-center space-x-3 ">
+      <header className="container mx-auto px-5 flex justify-between py-4 items-center">
+        <Link to="/" className="flex items-center space-x-3">
           <img className="w-10" src={logo} alt="logo" />
           <span className="hidden md:inline-block md:font-bold md:text-2xl">
             Spring
@@ -128,7 +157,7 @@ const Header = () => {
           } transition-all duration-300 mt-[56px] lg:mt-0 bg-primary lg:bg-white z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center overflow-y-auto lg:overflow-y-visible`}
         >
           <ul className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
-            {navItemsInfo.map((item) => (
+            {navItems.map((item) => (
               <NavItem key={item.name} item={item} />
             ))}
           </ul>
@@ -138,14 +167,14 @@ const Header = () => {
                 <div className="flex flex-col items-center">
                   <button
                     className="flex border-l-2 border-l-gray-500 pl-1 text-lg font-semibold hover:border-b-2 hover:text-blue-500 hover:border-blue-500 "
-                    onClick={() => setProfileDrowpdown(!profileDrowpdown)}
+                    onClick={() => setProfileDropdown(!profileDropdown)}
                   >
                     <span>Account</span>
                     <MdKeyboardArrowDown className="mt-2 ml-1" />
                   </button>
                   <div
                     className={`${
-                      profileDrowpdown ? "block" : "hidden"
+                      profileDropdown ? "block" : "hidden"
                     } lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
                   >
                     <ul className="bg-cta lg:bg-white text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
